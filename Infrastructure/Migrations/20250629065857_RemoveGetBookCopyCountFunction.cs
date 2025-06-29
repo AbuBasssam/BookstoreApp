0754e37da2b8863ee9f1c9be5a521aadb91bf7a7
Abdul Rahman <programmerabubassam@gmail.com>
2025-06-29 10:01:29 +0300

    removing the GetBookCopyCount function

---
+++ Infrastructure/Migrations/20250629065857_RemoveGetBookCopyCountFunction.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGetBookCopyCountFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS dbo.fn_GetBookCopyCount;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE FUNCTION dbo.fn_GetBookCopyCount(@BookID int)
+            RETURNS int
+            AS
+            BEGIN
+                DECLARE @count int;
+                SELECT @count = COUNT(*) FROM BookCopies WHERE BookID = @BookID;
+                RETURN ISNULL(@count, 0);
+            END
+            ");
+
+        }
+    }
+}
