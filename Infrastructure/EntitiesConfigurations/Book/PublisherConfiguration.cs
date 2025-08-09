a2d2a5a12c11e084331d3c9c8d346085b824511e
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-09 11:06:31 +0300

    feat(db): add Publisher entity FluentAPI configuration

---
+++ Infrastructure/EntitiesConfigurations/Book/PublisherConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntitiesConfigurations;
+
+internal class PublisherConfiguration : IEntityTypeConfiguration<Publisher>
+{
+    public void Configure(EntityTypeBuilder<Publisher> builder)
+    {
+        // Table
+        builder.ToTable("Publishers");
+
+        // Primary Key
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id)
+            .HasColumnName("PublisherID")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(p => p.NameEN)
+            .HasColumnType("NVARCHAR(50)")
+            .IsRequired();
+
+        builder.Property(p => p.NameAR)
+            .HasColumnType("NVARCHAR(50)")
+            .IsRequired();
+
+
+    }
+}
