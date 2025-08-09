2620c081816d46d0897ee94344c73bbf73dcec6e
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-09 10:32:10 +0300

    Create language entity class

---
+++ Domain/Entities/Book/Language.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+public class Language : IEntity<int>
+{
+    public int Id { get; set; }
+    public required string LanguageCode { get; set; }
+    public required string NameEN { get; set; }
+    public required string NameAR { get; set; }
+}
