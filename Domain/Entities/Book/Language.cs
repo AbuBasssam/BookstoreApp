6dfafc0f3977045865e14453dbe512fe8dd6e14f
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-09 10:51:34 +0300

    feat(db): add Language entity FluentAPI configuration and change LanguageCode property to Code

--- Domain/Entities/Book/Language.cs
+++ Domain/Entities/Book/Language.cs
@@ -2,7 +2,7 @@
 public class Language : IEntity<int>
 {
     public int Id { get; set; }
-    public required string LanguageCode { get; set; }
+    public required string Code { get; set; }
     public required string NameEN { get; set; }
     public required string NameAR { get; set; }
 }
---
+++ Infrastructure/EntitiesConfigurations/Book/LanguageConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntitiesConfigurations;
+internal class LanguageConfiguration : IEntityTypeConfiguration<Language>
+{
+    public void Configure(EntityTypeBuilder<Language> builder)
+    {
+        builder.ToTable("Languages");
+
+        builder.HasKey(l => l.Id);
+        builder.HasAlternateKey(l => l.Id).;
+
+        builder.Property(l => l.Id)
+            .HasColumnName("LanguageID")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(l => l.Code)
+            .HasColumnType("char(2)")
+            .IsRequired();
+
+        builder.Property(l => l.NameEN)
+            .HasColumnType("NVARCHAR(50)")
+            .IsRequired();
+
+
+        builder.Property(l => l.NameAR)
+            .HasColumnType("NVARCHAR(50)")
+            .IsRequired();
+
+        // Unique Constraint on Code
+        builder.HasIndex(l => l.Code)
+            .IsUnique()
+            .HasDatabaseName("UQ_Languages_Code");
+
+    }
+}
