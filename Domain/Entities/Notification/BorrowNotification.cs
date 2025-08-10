a546781d6bc58e9972f7283bfceec513f5800d21
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 17:22:22 +0300

    feat(db): add BorrwNotification class

---
+++ Domain/Entities/Notification/BorrowNotification.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class BorrowNotification
+{
+    public int Id { get; set; }
+    public int NotificationID { get; set; }
+    public int BorrowID { get; set; }
+
+    public virtual Notification Notification { get; set; }
+    public virtual BorrowingRecord Borrow { get; set; }
+}
