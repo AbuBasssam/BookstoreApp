294574fd8d07751034c7f48913dfad8988e2ba7f
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 16:13:19 +0300

    feat(db): Add BookRatingConfiguration class

---
+++ Infrastructure/EntitiesConfigurations/Book/BookRatingConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EntitiesConfigurations;
+
+public class BookRatingConfiguration : IEntityTypeConfiguration<BookRating>
+{
+    public void Configure(EntityTypeBuilder<BookRating> builder)
+    {
+        builder.ToTable("BookRatings");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("ID")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.BookID)
+            .IsRequired();
+
+        builder.Property(x => x.UserID)
+            .IsRequired();
+
+        builder.Property(x => x.Rating)
+            .HasColumnName("Rating")
+            .IsRequired();
+
+        builder.ToTable(br => br.HasCheckConstraint("CK_BookRating_Rating", "[Rating] >= 1 AND [Rating] <= 5"));
+
+        // Foreign key relationships
+        builder.HasOne(x => x.Book)
+            .WithMany()
+            .HasForeignKey(x => x.BookID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(x => x.BookID)
+            .HasDatabaseName("IX_BookRating_BookID");
+    }
+}
