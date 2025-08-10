4f3d1431f8c721f53450347005b8948b271c35b5
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 16:06:18 +0300

    feat(db): Add BookRating entity class

---
+++ Domain/Entities/Book/BookRating.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities;
+
+public class BookRating : IEntity<int>
+{
+    public int Id { get; set; }
+
+    public int BookID { get; set; }
+
+    public int UserID { get; set; }
+
+    public byte Rating { get; set; }
+
+    // Navigation properties
+    public virtual Book Book { get; set; }
+    public virtual User User { get; set; }
+}
