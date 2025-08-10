68bddd37393536caf684b1dd4744f7c139594a40
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 17:25:06 +0300

    feat(db): Add BorrowNotificationConfiguration class

---
+++ Infrastructure/EntitiesConfigurations/Notification/BorrowNotificationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntitiesConfigurations;
+
+public class BorrowNotificationConfiguration : IEntityTypeConfiguration<BorrowNotification>
+{
+    public void Configure(EntityTypeBuilder<BorrowNotification> builder)
+    {
+        builder.ToTable("BorrowNotification");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("ID")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.NotificationID)
+            .IsRequired();
+
+        builder.Property(x => x.BorrowID)
+            .IsRequired();
+
+        builder.HasOne(x => x.Notification)
+            .WithMany()
+            .HasForeignKey(x => x.NotificationID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Borrow)
+            .WithMany()
+            .HasForeignKey(x => x.BorrowID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
