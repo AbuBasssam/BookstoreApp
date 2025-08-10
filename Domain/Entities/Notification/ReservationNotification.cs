c9a72131dc155028b6c69c0daf91cf0a53c9034a
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 17:19:08 +0300

    feat(db):Add ReservationNotificationConfiguration class

--- Domain/Entities/Notification/ReservationNotification.cs
+++ Domain/Entities/Notification/ReservationNotification.cs
@@ -3,8 +3,8 @@
 public class ReservationNotification
 {
     public int Id { get; set; }
-    public int NotificationId { get; set; }
-    public int ReservationId { get; set; }
+    public int NotificationID { get; set; }
+    public int ReservationID { get; set; }
 
     public virtual Notification Notification { get; set; }
     public virtual ReservationRecord Reservation { get; set; }
---
+++ Infrastructure/EntitiesConfigurations/Notification/ReservationNotificationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntitiesConfigurations;
+
+public class ReservationNotificationConfiguration : IEntityTypeConfiguration<ReservationNotification>
+{
+    public void Configure(EntityTypeBuilder<ReservationNotification> builder)
+    {
+        builder.ToTable("ReservationNotification");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("ID")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.NotificationID)
+            .IsRequired();
+
+        builder.Property(x => x.ReservationID)
+            .IsRequired();
+
+        builder.HasOne(x => x.Notification)
+            .WithMany()
+            .HasForeignKey(x => x.NotificationID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Reservation)
+            .WithMany()
+            .HasForeignKey(x => x.ReservationID)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
