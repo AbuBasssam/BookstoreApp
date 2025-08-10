f7c6baf679aa6b3df76d6752dfbebc82a830de58
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 17:16:17 +0300

    feat(db):add ReservationNotification entity class

---
+++ Domain/Entities/Notification/ReservationNotification.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class ReservationNotification
+{
+    public int Id { get; set; }
+    public int NotificationId { get; set; }
+    public int ReservationId { get; set; }
+
+    public virtual Notification Notification { get; set; }
+    public virtual ReservationRecord Reservation { get; set; }
+}
