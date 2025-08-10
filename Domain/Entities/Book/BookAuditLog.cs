04c1dcfa22ad54ba06004af7574632ed89492b8f
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-10 15:48:12 +0300

    feat(db): add BookAuditLog entity class

---
+++ Domain/Entities/Book/BookAuditLog.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class BookAuditLog : IEntity<int>
+{
+       
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int? CopyId { get; set; }
+        public string? UpdatedFieldName { get; set; }
+        public string? OldValue { get; set; }
+        public string? NewValue { get; set; }
+        public BookActionType ActionType { get; set; }
+        public DateTime ActionDate { get; set; }
+        public int ByUserId { get; set; }
+
+        // Navigation properties
+        public virtual Book Book { get; set; }
+        public virtual BookCopy? Copy { get; set; }
+        public virtual User User { get; set; }
+    }
---
+++ Domain/Enums/BookActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums;
+
+public enum BookActionType : byte
+{
+    OpenReservation = 1,
+    CloseReservation = 2,
+    AddCopy = 3,
+    DeleteCopy = 4,
+    UpdateBookInfo = 5
+}
