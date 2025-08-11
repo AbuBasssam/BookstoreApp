70bfb880ccc75045650a3d537a92744ea6197ca0
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-11 06:48:41 +0300

    feat(db): complete Reservation & Borrowing module database implementation

---
+++ Infrastructure/Migrations/20250811033534_FixBorrwinghRecordsAdminIDColumn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBorrwinghRecordsAdminIDColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AdminID",
+                table: "BorrowingRecords",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+            migrationBuilder.AddForeignKey(
+                name: "FK_BorrowingRecords_Admins_AdminID",
+                table: "BorrowingRecords",
+                column: "AdminID",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
