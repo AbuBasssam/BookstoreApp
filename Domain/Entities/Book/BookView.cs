44f4559e4d8e9b64934e8cbef9225110d4a9cecf
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-17 14:54:46 +0300

    feat(db): Update book view to enhance version

--- Domain/Entities/Book/BookView.cs
+++ Domain/Entities/Book/BookView.cs
@@ -1,16 +1,29 @@
-﻿using Domain.Enums;
+﻿namespace Domain.Entities;
 
-namespace Domain.Entities;
-
-public class BookView: IEntity<int>
+public class BookView : IEntity<int>
 {
     public int Id { get; set; }
-    public string TitleEN { get; set; }
-    public string TitleAR { get; set; }
+    public string ISBN { get; set; } = null!;
+    public string TitleEN { get; set; } = null!;
+    public string TitleAR { get; set; } = null!;
+    public string DescriptionEN { get; set; } = null!;
+    public string DescriptionAR { get; set; } = null!;
+    public string PublisherEN { get; set; } = null!;
+    public string PublisherAR { get; set; } = null!;
+    public string AuthorEN { get; set; } = null!;
+    public string AuthorAR { get; set; } = null!;
+    public string LanguageEN { get; set; } = null!;
+    public string LanguageAR { get; set; } = null!;
+    public string CategoryEN { get; set; } = null!;
+    public string CategoryAR { get; set; } = null!;
+    public short PageCount { get; set; }
     public DateTime PublishDate { get; set; }
-    public string ISBN { get; set; }
-    public enCategory CategoryID { get; set; }
-    public int AuthorID { get; set; }
-    public string ImageUrl { get; set; }
-    public bool IsAvailable { get; set; } // True if ANY copy is available
+    public DateOnly AvailabilityDate { get; set; }
+    public string Position { get; set; } = null!;
+    public DateTime? LastWaitListOpenDate { get; set; }
+    public bool IsActive { get; set; }
+    public string CoverImage { get; set; } = null!;
+    public bool IsBorrowable { get; set; }
+    public bool IsReservable { get; set; }
+    public decimal Rating { get; set; }
 }
--- Infrastructure/EntitiesConfigurations/Book/BookViewConfiguration.cs
+++ Infrastructure/EntitiesConfigurations/Book/BookViewConfiguration.cs
@@ -9,20 +9,30 @@ public class BookViewConfiguration : IEntityTypeConfiguration<BookView>
     public void Configure(EntityTypeBuilder<BookView> builder)
     {
         builder.HasNoKey();
-        builder.ToView("BookView");
+        builder.ToView("vw_Books");
 
+        // Columns configuration
         builder.Property(b => b.Id).HasColumnName("BookId");
+        builder.Property(b => b.ISBN).HasMaxLength(20);
+        builder.Property(b => b.TitleEN).HasMaxLength(50);
+        builder.Property(b => b.TitleAR).HasMaxLength(50);
+        builder.Property(b => b.DescriptionEN).HasMaxLength(300);
+        builder.Property(b => b.DescriptionAR).HasMaxLength(300);
+        builder.Property(b => b.PublisherEN).HasMaxLength(50);
+        builder.Property(b => b.PublisherAR).HasMaxLength(50);
+        builder.Property(b => b.AuthorEN).HasMaxLength(50);
+        builder.Property(b => b.AuthorAR).HasMaxLength(50);
+        builder.Property(b => b.LanguageEN).HasMaxLength(50);
+        builder.Property(b => b.LanguageAR).HasMaxLength(50);
+        builder.Property(b => b.CategoryEN).HasMaxLength(50);
+        builder.Property(b => b.CategoryAR).HasMaxLength(50);
+        builder.Property(b => b.Position).HasMaxLength(20);
+        builder.Property(b => b.CoverImage).HasMaxLength(250);
 
-        builder.Property(b => b.TitleEN).HasColumnType("nvarchar").IsRequired().HasMaxLength(50);
-
-        builder.Property(b => b.ISBN).HasColumnType("nvarchar").IsRequired().HasMaxLength(20);
-
-        builder.Property(b => b.ImageUrl).HasColumnType("nvarchar")
-           .IsRequired()
-           .HasMaxLength(250);
-
-
-        builder.Property(b => b.TitleAR).HasColumnType("nvarchar").IsRequired().HasMaxLength(50);
+        // Computed columns
+        builder.Property(b => b.IsBorrowable);
+        builder.Property(b => b.IsReservable);
+        builder.Property(b => b.Rating).HasColumnType("decimal(3,2)");
 
     }
 }
