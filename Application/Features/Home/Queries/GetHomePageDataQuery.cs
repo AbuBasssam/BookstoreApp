a21c2b305c99cf0aa220a7604fb0b1eab0ec1f2e
Abdul Rahman <programmerabubassam@gmail.com>
2025-08-25 15:35:30 +0300

    feat(HomePage): Add GetHomePageDataQueryHandler class skeleton

--- Application/Features/Home/Queries/GetHomePageDataQuery.cs
+++ Application/Features/Home/Queries/GetHomePageDataQuery.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Models;
+using MediatR;
 
 namespace Application.Features.Home;
-public record GetHomePageDataQuery() : IRequest<HomePageResponseDto>;
+public record GetHomePageDataQuery() : IRequest<Response<HomePageResponseDto>>;
---
+++ Application/Features/Home/Queries/GetHomePageDataQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Domain.HelperClasses;
+using MediatR;
+
+namespace Application.Features.Home;
+
+public class GetHomePageDataQueryHandler : IRequestHandler<GetHomePageDataQuery, Response<HomePageResponseDto>>
+{
+    private readonly ICacheService _cacheService;
+    private readonly HomePageSettings _settings;
+
+    public GetHomePageDataQueryHandler(ICacheService cacheService, HomePageSettings settings)
+    {
+        _cacheService = cacheService;
+        _settings = settings;
+    }
+
+    public async Task<Response<HomePageResponseDto>> Handle(GetHomePageDataQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+
+    }
+}
