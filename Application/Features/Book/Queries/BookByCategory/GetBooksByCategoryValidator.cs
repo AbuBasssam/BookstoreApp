eeeb822ae4cdbf965c79172d240bdd232012ee38
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-13 19:59:56 +0300

    feat(book): Complete book feature refactor

---
+++ Application/Features/Book/Queries/BookByCategory/GetBooksByCategoryValidator.cs
@@ -0,0 +1,11 @@
+﻿using Application.Validations;
+
+namespace Application.Features.Book;
+
+public class GetBooksByCategoryValidator : LocalizePaginationValidator<GetNewBooksQuery>
+{
+    public GetBooksByCategoryValidator() : base()
+    {
+
+    }
+}
